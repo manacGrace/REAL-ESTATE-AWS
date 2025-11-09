92c3dccc84be13e99f47a2ebc655f21b3a0c8284
“ManasseTegGbegnohou” <mtgbegnohou@gmail.com>
2025-11-09 16:37:21 -0500

    manasse cs update

--- REAL-ESTATE-AWS-SERVICES/ProjetImmo_FE/src/pages/homepage/AboutManasse.jsx
+++ REAL-ESTATE-AWS-SERVICES/ProjetImmo_FE/src/pages/homepage/AboutManasse.jsx
@@ -8,31 +8,31 @@ function AboutManasse() {
   return (
     <div className="divManasse">
       <Container>
-        <Row className="rowManasse">
-          <Col xs={12} md={6}>
-            <h1 id="titleTextManasse">
-              Manassé <br /> Teg <br /> Gbegnohou
-            </h1>
-            <p id="textManasse">
-              Nous développons un site web de vente de maisons. 
-              Notre projet permet aux utilisateurs de consulter et explorer 
-              différentes propriétés à vendre.  <br /> 
-              Nous sommes une équipe de trois étudiants.
-            </p>
+        <Row>
+          <Col xs={8}>
+            <div>
+              <h1 id="titleTextManasse">
+                Manassé <br /> Teg <br /> Gbegnohou
+              </h1>
+              <p id="textManasse">
+                Nous développons un site web de vente de maisons. 
+                Notre projet permet aux utilisateurs de consulter et explorer 
+                différentes propriétés à vendre.  <br /> 
+                Nous sommes une équipe de trois étudiants.
+              </p>
+            </div>
           </Col>
-          <Col xs={12} md={6} className="colImageManasse">
-            <div className="image-wrapper-manasse">
-              <img
-                className="imagesManasse"
-                src="https://raw.githubusercontent.com/manacGrace/REAL-ESTATE-AWS/refs/heads/main/REAL-ESTATE-AWS-SERVICES/seed/pictures/picsAboutUs/picsManasse/manasse07.jpg?raw=true"
-                alt="Image Manasse"
-              />
-              <div className="github-link-container">
-                <i className="bx bxl-github"></i>
-                <a href="https://github.com/manacGrace" target="_blank" rel="noopener noreferrer" className="github-username">
-                  @manacGrace
-                </a>
-              </div>
+          <Col>
+            <img
+              className="imagesManasse"
+              src="https://raw.githubusercontent.com/manacGrace/REAL-ESTATE-AWS/refs/heads/main/REAL-ESTATE-AWS-SERVICES/seed/pictures/picsAboutUs/picsManasse/manasse07.jpg?raw=true"
+              alt="Image Manasse"
+            />
+            <div className="github-link-container">
+              <i className="bx bxl-github"></i>
+              <a href="https://github.com/manacGrace" target="_blank" rel="noopener noreferrer" className="github-username">
+                @manacGrace
+              </a>
             </div>
           </Col>
         </Row>
